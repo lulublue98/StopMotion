d138dfe09e3f167924902697542058fea5abf0f2
lulublue98 <lulu.jiyun.cho@gmail.com>
2015-01-06 23:39:40 -0500

    more stuff

---
+++ Project/Gui.java
@@ -0,0 +1,81 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui {
+
+    private JFrame frame;
+    private JButton clear;
+    private TestCanvas canvas;
+    private JPanel panel, panel2;
+    private CheckboxGroup options;
+    private Checkbox Linebox;
+    private Checkbox Drawbox;
+
+    /*
+    public void actionPerformed(ActionEvent e) {
+	if (e.getSource() == clear) {
+	    canvas.clear();
+	    canvas.paintComponent(canvas.getGraphics());
+	}
+	else if (e.getSource() == quit) {
+	    System.exit(0);
+	}
+	else if (e.getSource() == doStuff) {
+	    String opt = options.getSelectedCheckbox().getLabel();
+	    int n;
+	    if (opt.equals("Triple"))
+		n=3;
+	    else if (opt.equals("Double"))
+		n = 2;
+	    else
+		n=1;
+	    String s = fnamefield.getText();
+	    for (int i=0;i<n;i++)
+		System.out.println(i+":"+s);
+	    System.out.println();
+	}
+    }
+    */
+
+    public Gui() {
+	
+	options = new CheckboxGroup();
+	Linebox = new Checkbox("Line",options,true);
+	Drawbox = new Checkbox("Freehand",options,true);
+	
+	frame = new JFrame("Test Gui");
+	frame.setSize(1000,700);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	panel = new JPanel(new GridBagLayout());
+	panel.setPreferredSize(new Dimension(1000,200));
+	frame.getContentPane().add(panel, BorderLayout.NORTH);
+	
+	canvas = new TestCanvas(options);
+	frame.getContentPane().add(canvas, BorderLayout.WEST);
+	canvas.setPreferredSize(new Dimension(600,500));
+	canvas.setBorder(BorderFactory.createLineBorder(Color.blue, 5));
+	
+	panel2 = new JPanel(new GridBagLayout());
+	panel2.setPreferredSize(new Dimension(400,200));
+	frame.getContentPane().add(panel2, BorderLayout.EAST);
+	panel2.setBorder(BorderFactory.createLineBorder(Color.red, 5));
+	
+	GridBagConstraints c = new GridBagConstraints();
+	c.insets = new Insets(40,40,70,70);
+	GridBagConstraints c2 = new GridBagConstraints();
+	c2.insets = new Insets(40,40,0,0);
+	
+	c.gridx = 0;
+	c.gridy = 0;
+	panel2.add(Linebox, c);
+	c.gridy = 1;
+	panel2.add(Drawbox, c);
+
+    }
+
+    public static void main(String[] args) {
+	Gui T = new Gui();
+    }
+}
---
+++ Project/Line.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.imageio.*;
+public class Line {
+
+    public int x0,y0,x1,y1;
+    public Color c;
+
+    public Line(int x0,int y0, int x1, int y1, Color c) {
+	this.x0 = x0; this.y0 = y0;
+	this.x1 = x1; this.y1 = y1;
+	this.c = c;
+    }
+
+}

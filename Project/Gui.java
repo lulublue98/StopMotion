1581ba6dd74626d253a48bc54e899d7e9d63ec68
serenadipity <serenagchan@gmail.com>
2015-01-22 08:17:42 -0500

    Updates

--- Project/Gui.java
+++ Project/Gui.java
@@ -51,7 +51,7 @@ public class Gui implements ActionListener {
 	    canvas.setBackgroundColor( bgopt );
 	} else if ( e.getSource() == background ) {
 	    int index = background.getSelectedIndex();
-	    System.out.println(index);
+	    bgopt = bgcolors[index];
 	}
     }
 
